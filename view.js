3e1f77cd91ac48fb98743df1af4e2ac9e007b014
siva <selvamsiva4@gmail.com>
2025-07-31 11:51:18 +0530

    commit changes

--- view.js
+++ view.js
@@ -1,10 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router();                
 const { getAllFood, addFoodItem, updateFoodItem, deleteFoodItem } = require('../Controllers/foodController');
-
 router.get('/', getAllFood);
 router.post('/', addFoodItem);
-router.put('/:id', updateFoodItem);
-router.delete('/:id', deleteFoodItem);
-module.exports = router;
-
